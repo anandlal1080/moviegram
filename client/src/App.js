e0dae6558b5343f74e5516cf98ad088d46afcfba
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2021-01-21 13:40:38 -0700

    Made styles

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,17 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import icon from "./images/movie-night.png";
+import useStyles from "./styles";
+
 const App = () => {
+  const classes = useStyles();
   return (
     <Container maxidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Moviegram
         </Typography>
-        <img src={icon} alt="icon" height="60" />
+        <img className={classes.image} src={icon} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
