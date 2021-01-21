a9d453697f0408d417758096df1bcacbcf4ac752
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2021-01-21 12:00:06 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+import postRoutes from "./routes/posts.js";
+
+require("dotenv").config();
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
+
+mongoose.set("useFindAndModify", false);
---
+++ server/models/postMessage.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+  title: String,
+  synopsis: String,
+  creator: String,
+  tags: [String],
+  imageUrl: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  dislikeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const PostMessage = mongoose.model("PostMessage", postSchema);
+
+export default PostMessage;
