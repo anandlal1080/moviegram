38a78141384021388cda823ced114dab972a8032
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2021-01-21 14:33:08 -0700

    Made controller

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 import useStyles from "./styles";
 const Posts = () => {
+  const posts = useSelector((state) => state.posts);
   const classes = useStyles();
+  console.log(posts);
 
   return (
     <>
--- server/index.js
+++ server/index.js
@@ -11,11 +11,10 @@ require("dotenv").config();
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/posts", postRoutes);
 
 const PORT = process.env.PORT || 5000;
 
