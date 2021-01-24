db4c733b32bf7fde511cab582bd553358bae9cd0
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2021-01-23 22:44:56 -0700

    Updated reactions

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container } from "@material-ui/core";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
 
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useHistory, useLocation } from "react-router-dom";
 import { AppBar, Avatar, Button, Typography, Toolbar } from "@material-ui/core";
 import { useDispatch } from "react-redux";
-
+import decode from "jwt-decode";
 import useStyles from "./styles";
 import icon from "../../images/movie-night.png";
 
@@ -21,6 +21,12 @@ const Navbar = () => {
 
   useEffect(() => {
     const token = user?.token;
+
+    if (token) {
+      const decodedToken = decode(token);
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
+
     setUser(JSON.parse(localStorage.getItem("profile")));
   }, [location]);
 
